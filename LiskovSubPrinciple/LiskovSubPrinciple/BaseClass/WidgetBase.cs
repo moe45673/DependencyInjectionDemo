526c620b4e6bb44a096cc50cf25e8164d45176eb
Moshe Tenenbaum <mtenenbaum@survalent.com>
2019-09-24 12:12:16 -0400

    Initial Code for LSP

---
+++ LiskovSubPrinciple/LiskovSubPrinciple/BaseClass/WidgetBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubPrinciple.BaseClass
+{
+    public abstract class WidgetBase
+    {
+        public virtual IList<int> SomeList(string title)
+        {
+            //Some useful boilerplate implementation
+            return new List<int>();
+        }
+    }
+}
---
+++ LiskovSubPrinciple/LiskovSubPrinciple/ConcreteClass/BrokenWidget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiskovSubPrinciple.BaseClass;
+
+namespace LiskovSubPrinciple.ConcreteClass
+{
+    public class BrokenWidget : WidgetBase
+    {
+        //New Argument Type
+        public override IList<int> SomeList(object title)
+        {
+            return new List<int>();
+        }
+
+        //New Return Type
+        public override IList<object> SomeList(string title)
+        {
+            return new List<object>();
+        }
+    }
+}
